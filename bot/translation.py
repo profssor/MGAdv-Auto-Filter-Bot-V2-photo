901dea3f35fccac9baf23697a07fe342ab8006c8
Professor <81632023+wafikhsalman@users.noreply.github.com>
2021-05-04 19:57:46 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -7,9 +7,9 @@ class Translation(object):
     START_TEXT = """<b>Hey {}!!</b>
 <i>Am Just A Advance Auto Filter Bot....😁
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
 
 For More Details Click Help Button Below..
+@MGMOVIEGRAM
 </i>"""    
     
     HELP_TEXT = """
