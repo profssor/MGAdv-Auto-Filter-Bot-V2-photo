73c44f96da7a8184c2e32f00d5d847ff981fcbd2
Professor <81632023+wafikhsalman@users.noreply.github.com>
2021-05-04 20:07:10 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -48,7 +48,7 @@ For More Details Click Help Button Below..
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/Wafikh">WaFiKh SaLmaN</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
