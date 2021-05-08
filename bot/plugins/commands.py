0da03177b3ba4073ccbbfb42a601ba69281f1c26
Professor <81632023+wafikhsalman@users.noreply.github.com>
2021-05-08 11:07:21 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -38,7 +38,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'JOIN CHANNEL', url="https://t.me/joinchat/nppwyzxMr8NhN2M9"
+                                    '⚠️JOIN CHANNEL⚠️', url="https://t.me/joinchat/nppwyzxMr8NhN2M9"
                                 )
                         ]
                     ]
@@ -57,7 +57,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'JOIN CHANNEL', url="https://t.me/joinchat/nppwyzxMr8NhN2M9"
+                                    '⚠️JOIN CHANNEL⚠️', url="https://t.me/joinchat/nppwyzxMr8NhN2M9"
                                 )
                         ]
                     ]
@@ -76,7 +76,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'JOIN CHANNEL', url="https://t.me/joinchat/nppwyzxMr8NhN2M9"
+                                    '⚠️JOIN CHANNEL⚠️', url="https://t.me/joinchat/nppwyzxMr8NhN2M9"
                                 )
                         ]
                     ]
